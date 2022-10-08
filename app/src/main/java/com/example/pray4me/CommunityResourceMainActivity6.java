65d404db7efd5de00c2a4d49def44ea45f06b966
cost6 <cost.6@wright.edu>
2022-10-08 15:17:45 -0400

    Set text to proper size and font Everything works

--- app/src/main/java/com/example/pray4me/CommunityResourceMainActivity6.java
+++ app/src/main/java/com/example/pray4me/CommunityResourceMainActivity6.java
@@ -1,12 +1,12 @@
 package com.example.pray4me;
 
-import static com.example.pray4me.R.id.addiction;
-import static com.example.pray4me.R.id.cancer;
-import static com.example.pray4me.R.id.chainButton;
-import static com.example.pray4me.R.id.communityButton;
-import static com.example.pray4me.R.id.prayRequestImageButton;
-import static com.example.pray4me.R.id.request;
-import static com.example.pray4me.R.id.trouble;
+//import static com.example.pray4me.R.id.addiction;
+//import static com.example.pray4me.R.id.cancer;
+//import static com.example.pray4me.R.id.chainButton;
+//import static com.example.pray4me.R.id.communityButton;
+//import static com.example.pray4me.R.id.prayRequestImageButton;
+//import static com.example.pray4me.R.id.request;
+//import static com.example.pray4me.R.id.trouble;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -15,36 +15,40 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
-public class CommunityResourceMainActivity6 extends AppCompatActivity implements View.OnClickListener {
+public class CommunityResourceMainActivity6 extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_community_resource_main6);
 
-        TextView tvTrouble = null;
-        tvTrouble.setOnClickListener(this);
+        //TextView tvTrouble = null;
+        //tvTrouble.setOnClickListener(this);
 
     }
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
-            case trouble:
-                // make a new textview to represent the xml one
-                TextView myview = v.findViewById(trouble);
-                // get the string for the text view
-                String resource = getResources().getString(R.string.trouble);
-                //set the text for the request box
-                myview.setText(resource);
 
-                break;
-            case addiction:
-
-                break;
-            case cancer:
-
-                break;
-        }
     }
-}
+
+//    @Override
+//    public void onClick(View v) {
+//        switch (v.getId()){
+//            case trouble:
+//                // make a new textview to represent the xml one
+//                TextView myview = v.findViewById(trouble);
+//                // get the string for the text view
+//                String resource = getResources().getString(R.string.trouble);
+//                //set the text for the request box
+//                myview.setText(resource);
+//
+//                break;
+//            case addiction:
+//
+//                break;
+//            case cancer:
+//
+//                break;
+//        }
+    }
