5121848a2690b568c7471408624a8ee6c2f89d5c
cost6 <cost.6@wright.edu>
2023-07-05 21:58:29 -0400

    Add contact page and put the contact information into a JSon body

--- app/src/main/java/com/example/pray4me/addContact.java
+++ app/src/main/java/com/example/pray4me/addContact.java
@@ -25,7 +25,7 @@ public class addContact extends AppCompatActivity  implements View.OnClickListen
     // and store name of courses
     String[] usertypes= { "requester", "prayer",
             "admin", "resource"};
-
+    String usertype = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,53 +39,50 @@ public class addContact extends AppCompatActivity  implements View.OnClickListen
         // add a listener so the program will go to the onclick method when the button is clicked
         btnContactDone.setOnClickListener(this);
 
-    }
-
-    @Override
-    public void onClick(View v)
-    {
         Spinner username = findViewById(R.id.spinner);
         Object listener = null;
         username.setOnItemSelectedListener((AdapterView.OnItemSelectedListener) this);
 
         // Create the instance of ArrayAdapter
         // having the list of courses
-        ArrayAdapter ad
-                = new ArrayAdapter(
-                this,
-                android.R.layout.simple_spinner_item,
-                usertypes);
+        ArrayAdapter ad = new ArrayAdapter(this, android.R.layout.simple_spinner_item, usertypes);
 
         // set simple layout resource file
         // for each item of spinner
-        ad.setDropDownViewResource(
-                android.R.layout
-                        .simple_spinner_dropdown_item);
+        ad.setDropDownViewResource( android.R.layout.simple_spinner_dropdown_item);
 
         // Set the ArrayAdapter (ad) data on the
         // Spinner which binds data to spinner
         username.setAdapter(ad);
+    }
 
-
-
-//        EditText fname = findViewById(R.id.et_firstname);
-//        EditText lname = findViewById(R.id.et_firstname);
-//        EditText add1 = findViewById(R.id.et_firstname);
-//        EditText add2 = findViewById(R.id.et_firstname);
-//        EditText city = findViewById(R.id.et_firstname);
-//        EditText state = findViewById(R.id.et_firstname);
-//        EditText zipcode = findViewById(R.id.et_firstname);
-//        EditText phone = findViewById(R.id.et_firstname);
-//        EditText status = findViewById(R.id.et_firstname);
-//        EditText sex = findViewById(R.id.et_firstname);
-
-
-
-        //String test = fname.getEditableText().toString();
+    @Override
+    public void onClick(View v)
+    {
 
         JSONObject jsonBody = new JSONObject();
         try {
-            jsonBody.put("fname", "Bob");
+            EditText fname = findViewById(R.id.et_firstname);
+            EditText lname = findViewById(R.id.et_lastname);
+            EditText add1 = findViewById(R.id.et_address1);
+            EditText add2 = findViewById(R.id.et_address2);
+            EditText city = findViewById(R.id.et_city);
+            EditText state = findViewById(R.id.et_state);
+            EditText zipcode = findViewById(R.id.et_zipcode);
+            EditText phone = findViewById(R.id.et_phone);
+
+            jsonBody.put("fname", fname.getText());
+            jsonBody.put("lname", lname.getText());
+            jsonBody.put("address1", add1.getText());
+            jsonBody.put("address2", add2.getText());
+            jsonBody.put("city", city.getText());
+            jsonBody.put("state", state.getText());
+            jsonBody.put("zipcode", zipcode.getText());
+            jsonBody.put("phone", phone.getText());
+            jsonBody.put("usertype", usertype);
+
+            String stophere = "";
+
         } catch (JSONException e) {
             throw new RuntimeException(e);
         }
@@ -102,6 +99,7 @@ public class addContact extends AppCompatActivity  implements View.OnClickListen
                         usertypes[position],
                         Toast.LENGTH_LONG)
                 .show();
+        usertype = usertypes[position];
     }
 
     @Override
