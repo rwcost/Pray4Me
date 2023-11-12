e581091480a005c6cde47dc07846122029a15be8
cost6 <cost.6@wright.edu>
2023-11-12 17:47:26 -0500

    Enable the select pray request screen to scroll

--- app/src/main/java/com/example/pray4me/SelectRequestJava.java
+++ app/src/main/java/com/example/pray4me/SelectRequestJava.java
@@ -6,14 +6,11 @@ import android.annotation.SuppressLint;
 import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import static com.example.pray4me.R.id.btnSelectToPray;
 import static com.example.pray4me.R.id.btnPrayForAll;
 import static com.example.pray4me.R.id.btnRandomRequest;
 import static com.example.pray4me.R.id.btnPrayByZipcode;
